9f51390067893bafe9a74dc1d57e59ea5a6ccee6
Umar Kabir <61500640+omar-kabeer@users.noreply.github.com>
2023-06-25 18:44:14 +0100

    updating medels

---
+++ README.md
@@ -0,0 +1,48 @@
+# Welcome to AI Atari Game Player
+***
+
+## Task
+The task of this project is to build an AI that can play Atari video games using reinforcement learning and the Deep Q-Network (DQN) approach. The challenge lies in training the AI models to perform better than humans in playing the games, considering the diverse screens and complexities of the Atari games.
+
+## Description
+In this project, we have implemented three different models to solve three different Atari games: Cart Pole, Space Invaders, and Pacman. We have used neural networks and reinforcement learning techniques, specifically the DQN algorithm, to train the models.
+
+The project begins with setting up the OpenAI Gym environment and installing the required libraries. We then proceed to train each model separately by following the steps specific to each game. The models are built using neural networks, and the DQN algorithm is used to optimize the action-value function.
+
+## Installation
+To install and run this project, follow these steps:
+
+1. Clone the repository:
+
+2. Install the required dependencies:
+
+
+## Usage
+To run the project, execute the following command in the project directory:
+
+Make sure you have the necessary GPU support and drivers installed to leverage GPU acceleration for training the neural networks.
+
+The project will train the models on the respective games and save the trained models as files. You can then evaluate the performance of the models, test them against new game instances, or use them for further research and development.
+
+
+Feel free to modify the code, experiment with different architectures or hyperparameters, and explore other Atari games.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+This project is licensed under the [MIT License](LICENSE).
+
+## Acknowledgments
+- The OpenAI Gym community for providing the Atari game environments.
+- The authors of the DQN algorithm for their valuable research.
+- [OpenAI Gym Documentation](https://gym.openai.com/docs/) for the comprehensive guides.
+
+You can copy and paste this code into your README.md file. Feel free to modify the content to fit your project's specific details and add any additional sections if necessary.
+
+
+### The Core Team
+Umar Kabir
+
+<span><i>Made at <a href='https://qwasar.io'>Qwasar SV -- Software Engineering School</a></i></span>
+<span><img alt='Qwasar SV -- Software Engineering School's Logo' src='https://storage.googleapis.com/qwasar-public/qwasar-logo_50x50.png' width='20px'></span>
---
+++ cartpole.py
@@ -0,0 +1,131 @@
+import numpy as np
+import gym
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.optimizers import Adam
+
+class DQN:
+    def __init__(self, state_space, action_space):
+        self.state_space = state_space
+        self.action_space = action_space
+        self.memory = []
+        self.gamma = 0.95  # discount factor
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.model = self.build_model()
+
+    def build_model(self):
+        model = Sequential()
+        model.add(Flatten(input_shape=self.state_space))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_space, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(learning_rate=0.001))
+        return model
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return np.random.randint(self.action_space)
+        q_values = self.model.predict(state)
+        return np.argmax(q_values[0])
+    def experience_replay(self, batch_size):
+        """Store and sample experiences from the environment."""
+        # Store the most recent experiences in a replay buffer
+        for state, action, reward, next_state, done in self.memory:
+            self.replay_buffer.append((state, action, reward, next_state, done))
+
+        # Sample a random batch of experiences from the replay buffer
+        batch = self.replay_buffer.sample(batch_size)
+
+        # Update the Q-network using the sampled experiences
+        self.update_q_network(batch)
+
+    def update_exploration_rate(self):
+        self.epsilon = self.epsilon * self.epsilon_decay
+        if self.epsilon < self.epsilon_min:
+            self.epsilon = self.epsilon_min
+
+
+    # ... (rest of the code remains the same)
+
+
+# Create the environment
+env = gym.make('CartPole-v0')
+
+state_space = env.observation_space.shape
+action_space = env.action_space.n
+
+# Create the DQN agent
+agent = DQN(state_space, action_space)
+
+# Training loop
+batch_size = 64
+episodes = 100
+
+for episode in range(episodes):
+    state = env.reset()
+    done = False
+    total_reward = 0
+
+    while not done:
+        env.render()
+        # Preprocess state (if necessary) and reshape it
+        state = np.expand_dims(state, axis=0)
+
+        # Choose action
+        action = agent.act(state)
+
+        # Take the action in the environment
+        step_result = env.step(action)
+        next_state = step_result[0]
+        reward = step_result[1]
+        done = step_result[2]
+
+
+        # Preprocess next_state (if necessary) and reshape it
+        next_state = np.expand_dims(next_state, axis=0)
+
+        # Store the experience in memory
+        agent.remember(state, action, reward, next_state, done)
+
+        # Update the state
+        state = next_state
+        total_reward += reward
+
+    # Perform experience replay
+    agent.experience_replay(batch_size)
+
+    # Update exploration rate
+    agent.update_exploration_rate()
+
+    # Print episode statistics
+    print(f"Episode: {episode+1}, Total Reward: {total_reward}")
+
+# Save the trained model
+agent.model.save('cartpole_dqn_model.h5')
+
+# Evaluate the trained model
+env = gym.make('CartPole-v1')
+
+# Load the trained model
+model = tf.keras.models.load_model('cartpole_dqn_model.h5')
+
+# Evaluate the model for 10 episodes
+total_reward = 0
+for episode in range(10):
+    state = env.reset()
+    done = False
+    while not done:
+        action = np.argmax(model.predict(state)[0])
+        step_result = env.step(action)
+        next_state = step_result[0]
+        reward = step_result[1]
+        done = step_result[2]
+        state = next_state
+        total_reward += reward
+
+print(f"Average reward: {total_reward / 10}")
